65e154d7024164ca9d29630f6d425290d1521bfa
heweidong <heweidong526@gmail.com>
2015-11-06 18:41:45 +0800

    add c language tokens

---
+++ src/frontend/clang/ClangTokenType.java
@@ -0,0 +1,112 @@
+package frontend.clang;
+
+import frontend.TokenType;
+
+import java.util.HashSet;
+import java.util.Hashtable;
+
+/**
+ * <h1>ClangTokenType</h1>
+ *
+ * <p>C language token types.</p>
+ */
+public enum ClangTokenType implements TokenType {
+  /* Reserved words. */
+
+  // type specifier
+  VOID, CHAR, SHORT, INT, LONG, FLOAT, DOUBLE, SIGNED, UNSIGNED,
+  ENUM, UNION, STRUCT,
+
+  // type qualifier
+  CONST, VOLATILE,
+
+  // storage class
+  TYPEDEF, EXTERN, STATIC, AUTO, REGISTER,
+
+  // control flow keyword
+  IF, ELSE, DO, WHILE, FOR, GOTO, CONTINUE, BREAK,
+  SWITCH, CASE, RETURN,
+
+  // other keywords
+  DEFAULT,  SIZEOF,
+
+  // basic operators
+  ADD("+"), SUB("-"), MUL("*"), DIV("/"), MOD("%"),
+  ADD_ASSIGN("+="), SUB_ASSIGN("-="), MUL_ASSIGN("*="), DIV_ASSIGN("/="), MOD_ASSIGN("%="),
+
+  // relational operators
+  G_OP(">"), L_OP("<"),  GE_OP(">="), LE_OP("<="), EQ_OP("=="), NE_EQ("!="),
+
+  // bit operators
+  BIT_AND("&"), BIT_XOR("^"), BIT_OR("|"), BIT_NOT("~"),
+  BIT_AND_ASSIGN("&="), BIT_XOR_ASSIGN("^="), BIT_OR_ASSIGN("|="),
+
+  // shift operators
+  LEFT_SHIFT("<<"), RIGHT_SHIFT(">>"),
+  LEFT_SHIFT_ASSIGN("<<="), RIGHT_SHIFT_ASSIGN(">>="),
+
+  // logical operators
+  LOGICAL_NOT("!"), LOGICAL_AND("&&"), LOGICAL_OR("||"),
+
+  // condition operator
+  CONDITION("?"),
+
+  // other operators
+  ASSIGN("="), INC_OP("++"), DEC_OP("--"), PTR_OP("->"), DOT("."),
+  ELLIPSIS("..."),
+
+  // others symbols
+  COLON(":"), COMMA(","), SEMICOLON(";"),
+  LEFT_PAREN("("), RIGHT_PAREN(")"), LEFT_BRACKET("["), RIGHT_BRACKET("]"),
+  LEFT_BRACE("{"), RIGHT_BRACE("}");
+
+  private String text; // token text
+
+  /**
+   * Constructor.
+   */
+  private ClangTokenType() {
+    this.text = this.toString().toLowerCase();
+  }
+
+  /**
+   * Constructor.
+   * @param text the token text
+   */
+  private ClangTokenType(String text) {
+    this.text = text;
+  }
+
+  /**
+   * Getter.
+   * @return the token text.
+   */
+  public String getText() {
+    return text;
+  }
+
+  private static final int FIRST_RESERVED_INDEX = VOID.ordinal();
+  private static final int LAST_RESERVED_INDEX = SIZEOF.ordinal();
+
+  // Set of lower-cased C reserved word text strings.
+  public static HashSet<String> RESERVED_WORDS = new HashSet<>();
+  static {
+    ClangTokenType[] values = ClangTokenType.values();
+    for (int i = FIRST_RESERVED_INDEX; i <= LAST_RESERVED_INDEX; ++i) {
+      RESERVED_WORDS.add(values[i].getText());
+    }
+  }
+
+  private static final int FIRST_SPECIAL_INDEX = ADD.ordinal();
+  private static final int LAST_SPECIAL_INDEX = RIGHT_BRACE.ordinal();
+
+  // Hash table of C language special symbols.
+  // Each special symbol's text is the key to its C language token type.
+  public static Hashtable<String, ClangTokenType> SPECIAL_SYMBOLS = new Hashtable<>();
+  static {
+    ClangTokenType[] values = ClangTokenType.values();
+    for (int i = FIRST_SPECIAL_INDEX; i <= LAST_SPECIAL_INDEX; ++i) {
+      SPECIAL_SYMBOLS.put(values[i].getText(), values[i]);
+    }
+  }
+}
