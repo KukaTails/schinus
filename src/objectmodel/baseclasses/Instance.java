93a7fda354949a8629ff2a3984a2d8bd65b41735
heweidong <heweidong526@gmail.com>
2015-11-21 00:25:22 +0800

    add instance class of object model

---
+++ src/objectmodel/baseclasses/Instance.java
@@ -0,0 +1,21 @@
+package objectmodel.baseclasses;
+
+import objectmodel.dictionary.Dictionary;
+
+/**
+ * <h1>Instance</h1>
+ *
+ * <p>Instance of a user-defined class</p>
+ */
+public class Instance extends Base {
+  /**
+   * Create a instance of a class, and the class should be a instance of Class.
+   * initialize the instance's field by using the dict of the className's field,
+   * and record the instance's class is className
+   *
+   * @param className a instance of class
+   */
+  public Instance(objectmodel.baseclasses.Class className, Dictionary fields, Dictionary parentDictionary) {
+    super(className, fields, parentDictionary);
+  }
+}
