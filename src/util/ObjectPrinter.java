ed4739e5f77295d9c403808852f43a821e889eb9
heweidong <heweidong526@gmail.com>
2015-11-20 17:14:12 +0800

    add utility for print object

---
+++ src/util/ObjectPrinter.java
@@ -0,0 +1,21 @@
+package util;
+
+import static objectmodel.predefined.PredefinedConstant.NONE;
+
+import objectmodel.baseclasses.Instance;
+
+public class ObjectPrinter {
+  public static void printValue(Object object) {
+    Object value = object;
+
+    if (object instanceof Instance) {
+      value = ((Instance)object).readAttribute("__value__");
+    }
+
+    if (!object.equals(NONE)) {
+      System.out.println(value);
+    } else {
+      System.out.println();
+    }
+  }
+}
