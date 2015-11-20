afb1aed94f35444ab212a1c96300950313544ed7
heweidong <heweidong526@gmail.com>
2015-11-20 13:44:51 +0800

    add methodTest

--- testing/src/parsesrtest/ExpressionStatementParserTest.java
+++ testing/src/parsesrtest/ExpressionStatementParserTest.java
@@ -37,76 +37,35 @@ public class ExpressionStatementParserTest {
   public void AtomParseTest() throws Exception {
     String[] testCases = {"identifier", "\'string\'", "23323", "3.456", "None", "True", "False"};
 
-    for (String testCase: testCases) {
-      Method parseAtomMethod = initParse(testCase, "parseAtom");
-      Token token = expressionParser.nextToken();
-      ICodeNode iCodeNode = (ICodeNode) parseAtomMethod.invoke(expressionParser, token);
-
-      Object result = executor.execute(iCodeNode, GLOBAL_ENV);
-      System.out.print(testCase + " = ");
-      ObjectPrinter.printValue(result);
-    }
+    MethodTest(testCases, "parseAtom");
   }
 
   @Test
   public void atomExprParseTest() throws Exception {
     String[] testCases = {"1234"};
 
-    for (String testCase : testCases) {
-      Method parseAtomExprMethod = initParse(testCase, "parseAtomExpr");
-      Token token = expressionParser.nextToken();
-
-      ICodeNode iCodeNode = (ICodeNode) parseAtomExprMethod.invoke(expressionParser, token);
-      Object result = executor.execute(iCodeNode, GLOBAL_ENV);
-      System.out.print(testCase + " = ");
-
-      ObjectPrinter.printValue(result);
-    }
+    MethodTest(testCases, "parseAtomExpr");
   }
 
   @Test
   public void parsePowerTest() throws Exception {
     String[] testCases = {"2 ** 3", "2.9833 ** 10", "10.11 ** 10.11"};
 
-    for (String testCase : testCases) {
-      Method method = initParse(testCase, "parsePower");
-      Token token = expressionParser.nextToken();
-
-      ICodeNode iCodeNode = (ICodeNode) method.invoke(expressionParser, token);
-      Object result = executor.execute(iCodeNode, GLOBAL_ENV);
-      System.out.print(testCase + " = ");
-      ObjectPrinter.printValue(result);
-    }
+    MethodTest(testCases, "parsePower");
   }
 
   @Test
   public void parseFactorTest() throws Exception {
     String[] testCases = {"+12", "-10", "-10.29", "--193", "--192.3", "++20132159.99"};
 
-    for (String testCase : testCases) {
-      Method method = initParse(testCase, "parseFactor");
-      Token token = expressionParser.nextToken();
-
-      ICodeNode iCodeNode = (ICodeNode)method.invoke(expressionParser, token);
-      Object result = executor.execute(iCodeNode, GLOBAL_ENV);
-      System.out.print(testCase + " = ");
-      ObjectPrinter.printValue(result);
-    }
+    MethodTest(testCases, "parseFactor");
   }
 
   @Test
   public void parseTermTest() throws Exception {
     String[] testCases = {"+12 * -10", "-10 / --1", "-10.29", "--193 // 2 // 4", "--192.3 * -10", "++20132159 % 10"};
 
-    for (String testCase : testCases) {
-      Method method = initParse(testCase, "parseTerm");
-      Token token = expressionParser.nextToken();
-
-      ICodeNode iCodeNode = (ICodeNode)method.invoke(expressionParser, token);
-      Object result = executor.execute(iCodeNode, GLOBAL_ENV);
-      System.out.print(testCase + " = ");
-      ObjectPrinter.printValue(result);
-    }
+    MethodTest(testCases, "parseTerm");
   }
 
 
@@ -119,14 +78,19 @@ public class ExpressionStatementParserTest {
         "2 == 2", "10.3 == (10 + 0.3)",
         "2 != 2.0",  "2 != 1 + 1"};
 
+    MethodTest(testCases, "parseComparisonExpression");
+  }
+
+  private void MethodTest(String[] testCases, String methodName) throws Exception {
     for (String testCase : testCases) {
-      Method method = initParse(testCase, "parseComparisonExpression");
+      Method method = initParse(testCase, methodName);
       Token token = expressionParser.nextToken();
 
       ICodeNode iCodeNode = (ICodeNode) method.invoke(expressionParser, token);
       Object result = executor.execute(iCodeNode, GLOBAL_ENV);
       System.out.print(testCase + " = ");
       ObjectPrinter.printValue(result);
+
     }
   }
 
