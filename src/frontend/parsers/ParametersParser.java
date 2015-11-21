d869d4c79725bebabf5bdbd1879f8ee20bb8ef5a
heweidong <heweidong526@gmail.com>
2015-11-21 21:04:34 +0800

    add token match

--- src/frontend/parsers/ParametersParser.java
+++ src/frontend/parsers/ParametersParser.java
@@ -1,8 +1,10 @@
 package frontend.parsers;
 
-import static frontend.TokenType.COLON;
+import static frontend.TokenType.COMMA;
+import static frontend.TokenType.IDENTIFIER;
+import static frontend.TokenType.LEFT_PAREN;
 import static frontend.TokenType.RIGHT_PAREN;
-import static frontend.ErrorCode.MISSING_RIGHT_PAREN;
+import static frontend.ErrorCode.UNEXPECTED_TOKEN;
 import static intermediate.ICodeKey.PARAMETERS_LIST;
 import static intermediate.ICodeNodeType.PARAMETERS;
 
@@ -29,6 +31,7 @@ public class ParametersParser extends Parser {
 
   /**
    * Parse parameter list of function define statement or function call statement.
+   *
    * @param token the current token to start parse.
    * @return the code node of if statement.
    * @throws Exception if an error occurred.
@@ -37,23 +40,28 @@ public class ParametersParser extends Parser {
     ICodeNode parametersNode = ICodeFactory.createICodeNode(PARAMETERS);
     ArrayList<String> parameters = new ArrayList<>();
 
-    token = nextToken();  // consume '('
-    while (token.getType() != RIGHT_PAREN && token.getType() != COLON) {
+    token = match(LEFT_PAREN);  // consume '('
+    while (token.getType() != RIGHT_PAREN) {
       parameters.add(token.getText());
-      token = nextToken(); // consume the identifier
-      if (token.getType() == RIGHT_PAREN || token.getType() == COLON)
+      token = match(IDENTIFIER); // consume the identifier
+      if (token.getType() == RIGHT_PAREN)
         break;
-      token = nextToken(); // consume '(' or ','
+
+      // consume '(' or ','
+      if (token.getType() == LEFT_PAREN)
+        token = match(LEFT_PAREN);
+      else if (token.getType() == COMMA)
+        token = match(COMMA);
+      else {
+        token = nextToken();
+        errorHandler.flag(token, UNEXPECTED_TOKEN, this);
+      }
     }
 
     // if there is no parameter, the size of parameters is zero.
     parametersNode.setAttribute(PARAMETERS_LIST, parameters);
 
-    // missing right paren of parameter list
-    if (token.getType() == RIGHT_PAREN) {
-      nextToken();  // consume ')'
-    } else
-      errorHandler.flag(token, MISSING_RIGHT_PAREN, this);
+    match(RIGHT_PAREN);  // consume ')'
     return parametersNode;
   }
 }
