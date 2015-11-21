a5f72968112b16c9c8355c535b9bc251d43f21ce
heweidong <heweidong526@gmail.com>
2015-11-21 21:07:38 +0800

    add executor class

---
+++ src/interpreter/Executor.java
@@ -0,0 +1,120 @@
+package interpreter;
+
+import static intermediate.ICodeNodeType.ATOM_EXPR_NODE;
+import static intermediate.ICodeNodeType.EXPRESSION_NODE;
+import static objectmodel.predefined.PredefinedConstant.NONE;
+import static objectmodel.predefined.PredefinedType.*;
+
+import message.Message;
+import message.MessageHandler;
+import message.MessageListener;
+import intermediate.ICodeNode;
+import objectmodel.baseclasses.Instance;
+import objectmodel.dictionary.Dictionary;
+import interpreter.executors.StmtExecutor;
+
+import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
+
+/**
+ * <h1>Executor</h1>
+ * <p>
+ * <p>The executor for an interpreter back end.</p>
+ */
+public class Executor {
+  protected static MessageHandler messageHandler;          // message handler delegate
+  protected static int executionCount;                     // count of execution statements
+  protected static RuntimeErrorHandler errorHandler;
+  protected static ArrayList<Instance> constantInstance;
+  protected static int constantId = 0;
+
+  static {
+    executionCount = 0;
+    errorHandler = new RuntimeErrorHandler();
+    messageHandler = new MessageHandler();
+    constantInstance = new ArrayList<>();
+  }
+
+  public Executor() {
+  }
+
+  /**
+   * @return the error handler.
+   */
+  public RuntimeErrorHandler getErrorHandler() {
+    return errorHandler;
+  }
+
+  /**
+   * Execute the source program by processing the intermediate code and the environment.
+   *
+   * @param iCodeNode   the intermediate code.
+   * @param environment the intermediate code executed in the environment.
+   * @throws Exception if an error occurred.
+   */
+  public void process(ICodeNode iCodeNode, Dictionary environment) throws Exception {
+    ExecuteResultPrinter executeResultPrinter = new ExecuteResultPrinter();
+
+    // Get the root node of the intermediate code and execute the node.
+    StmtExecutor statementExecutor = new StmtExecutor();
+    Object result = statementExecutor.execute(iCodeNode, environment);
+    if (iCodeNode.getType() == EXPRESSION_NODE)
+      executeResultPrinter.printObject(result);
+  }
+
+  /**
+   * Create constant instance in environment.
+   *
+   * @param value       the value of instance.
+   * @param environment the environment which contains the instance.
+   * @return the instance created.
+   */
+  public Instance createConstantInstance(Object value, Dictionary environment, Dictionary existedEnv) {
+    Instance instance = NONE;
+    int id = constantId++;
+
+    if (value instanceof String) {
+      instance = new Instance(STRING, new Dictionary(), environment, existedEnv);
+    } else if (value instanceof Integer) {
+      instance = new Instance(INTEGER, new Dictionary(), environment, existedEnv);
+    } else if (value instanceof Float || value instanceof Double) {
+      instance = new Instance(FLOAT, new Dictionary(), environment, existedEnv);
+    } else if (value instanceof Boolean) {
+      instance = new Instance(BOOLEAN, new Dictionary(), environment, existedEnv);
+    }
+    instance.setExistedEnv(existedEnv);
+    instance.writeAttr("__value__", value);
+    instance.writeAttr("__name__", "constant" + id);
+    constantInstance.add(instance);
+
+    return instance;
+  }
+
+  /**
+   * @return the message handler.
+   */
+  public MessageHandler getMessageHandler() {
+    return messageHandler;
+  }
+
+  /**
+   * @param message the message to set.
+   */
+  public void sendMessage(Message message) {
+    messageHandler.sendMessage(message);
+  }
+
+  /**
+   * @param listener the message listener to add.
+   */
+  public void addMessageListener(MessageListener listener) {
+    messageHandler.addListener(listener);
+  }
+
+  /**
+   * @param listener the message listener to remove.
+   */
+  public void removeMessageListener(MessageListener listener) {
+    messageHandler.removeListener(listener);
+  }
+}
