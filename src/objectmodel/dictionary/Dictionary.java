951f67fedcde337eafbf464384f6e3ab58461c39
heweidong <heweidong526@gmail.com>
2015-11-22 00:47:02 +0800

    fix bugs on read field from environment

--- src/objectmodel/dictionary/Dictionary.java
+++ src/objectmodel/dictionary/Dictionary.java
@@ -1,5 +1,6 @@
 package objectmodel.dictionary;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
 
 /**
@@ -13,6 +14,47 @@ public class Dictionary extends Hashtable<String, Object> {
   public Dictionary() {
     parentDictionary = null;
   }
+
+  public Object readField(String field){
+    Object result = get(field);
+
+    if (result != null) {
+      return result;
+    }
+    result = readFromSuperDict(field);
+    return result;
+  }
+
+
+  /**
+   * Get the object resolution dictionaries.
+   * @return
+   */
+  public ArrayList<Dictionary> fieldResolutionOrder() {
+    ArrayList<Dictionary> superDic = new ArrayList<>();
+
+    superDic.add(this);
+    if (parentDictionary != null) {
+      superDic.addAll(parentDictionary.fieldResolutionOrder());
+    }
+    return superDic;
+  }
+
+  /**
+   * Read a field from superDict
+   * @param field the field to read.
+   * @return field object.
+   */
+  public Object readFromSuperDict(String field) {
+    ArrayList<Dictionary> superDict = fieldResolutionOrder();
+
+    for (Dictionary dict : superDict) {
+      if (dict.containsKey(field))
+        return dict.get(field);
+    }
+    return null;
+  }
+
 
   /**
    * @param parent the parent dictionary which contains the dictionary.
