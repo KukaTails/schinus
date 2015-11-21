c5441512443eaaf3ad71758af23e71419f01fa92
heweidong <heweidong526@gmail.com>
2015-11-21 16:05:05 +0800

    delete Uninitialized value token

--- src/interpreter/RuntimeErrorCode.java
+++ src/interpreter/RuntimeErrorCode.java
@@ -6,11 +6,9 @@ package interpreter;
  * <p>Runtime error codes.</p>
  */
 public enum RuntimeErrorCode {
-  UNINITIALIZED_VALUE("Uninitialized value"),                // use uninitialized value
   DIVISION_BY_ZERO("Division by zero"),                      // division by zero
   UNDEFINED_NAME("Undefined name"),                           // use undefined name
   UNIMPLEMENTED_FEATURE("Unimplemented runtime feature"),    // unimplemented runtime feature
-
   CALL_UNCALLABLE_OBJECT("Object is not callable"),          // call a object which is not callable
   ASSIGN_TO_LEFT_VALUE("Can not assign to left value"),      // assign to left value
   OBJECT_IS_NOT_CLASS("Object is not class");                // use a object which is not class to be base class
