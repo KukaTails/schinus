c950bc3401a65f6a0a9c080b55d84634cf7c7a93
heweidong <heweidong526@gmail.com>
2015-11-22 23:43:36 +0800

    add return flow exception

--- src/interpreter/executors/WhileStmtExecutor.java
+++ src/interpreter/executors/WhileStmtExecutor.java
@@ -5,6 +5,7 @@ import static objectmodel.predefined.PredefinedConstant.NO_PRINT;
 
 import intermediate.ICodeNode;
 import intermediate.ICodeNodeType;
+import interpreter.exception.ReturnFlowException;
 import objectmodel.baseclasses.Instance;
 import objectmodel.dictionary.Dictionary;
 
@@ -12,7 +13,7 @@ import java.util.ArrayList;
 
 /**
  * <h1>WhileStatementExecutor</h1>
- *
+ * <p>
  * <p>While statement executor.</p>
  */
 public class WhileStmtExecutor extends StmtExecutor {
@@ -20,27 +21,30 @@ public class WhileStmtExecutor extends StmtExecutor {
   }
 
   /**
-   * @param iCodeNode the intermediate code node of while statement.
+   * @param iCodeNode   the intermediate code node of while statement.
    * @param environment the environment to execute while statement.
    * @return the result of executing while statement.
    */
   @Override
-  public Object execute(ICodeNode iCodeNode, Dictionary environment) {
+  public Object execute(ICodeNode iCodeNode, Dictionary environment)
+      throws ReturnFlowException {
+    ExprStmtExecutor exprStmtExecutor = new ExprStmtExecutor();
+    StmtExecutor stmtExecutor = new StmtExecutor();
+
     ICodeNode expressionNode = iCodeNode.getChildren().get(0);
     ICodeNode compoundStatementNode = iCodeNode.getChildren().get(1);
 
     try {
       while (true) {
-        ExprStmtExecutor expressionStatementExecutor = new ExprStmtExecutor();
-        Object expressionResult = expressionStatementExecutor.execute(expressionNode, environment);
+        Object expressionResult = exprStmtExecutor.execute(expressionNode, environment);
 
-        // if the result of executing condition expression is false,
-        // stop execute compound statement.
         if (!checkConditionResult(expressionResult))
           break;
-        executeCompoundStmt(compoundStatementNode, environment);
+        stmtExecutor.execute(compoundStatementNode, environment);
       }
-    }catch(Exception e) {
+    } catch(ReturnFlowException e) {
+      throw e;
+    } catch(Exception e) {
       System.out.println(e);
     }
     return null;
@@ -48,17 +52,16 @@ public class WhileStmtExecutor extends StmtExecutor {
 
   /**
    * Execute compound statement.
+   *
    * @param compoundNode intermediate code node of compound statement.
-   * @param environment environment to execute compound statement.
+   * @param environment  environment to execute compound statement.
    */
-  private Object executeCompoundStmt(ICodeNode compoundNode, Dictionary environment) {
+  private Object executeCompoundStmt(ICodeNode compoundNode, Dictionary environment) throws ReturnFlowException {
     ArrayList<ICodeNode> stmtsNode = compoundNode.getChildren();
     StmtExecutor stmtExecutor = new StmtExecutor();
     Object result = NO_PRINT;
     for (ICodeNode stmtNode : stmtsNode) {
       result = stmtExecutor.execute(stmtNode, environment);
-      if (stmtNode.getType() == RETURN_STATEMENT)
-        break;
     }
     return result;
   }
@@ -71,7 +74,7 @@ public class WhileStmtExecutor extends StmtExecutor {
    */
   private boolean checkConditionResult(Object conditionResult) {
     if (conditionResult instanceof Instance)
-      conditionResult = ((Instance)conditionResult).readAttribute("__value__");
+      conditionResult = ((Instance) conditionResult).readAttribute("__value__");
 
     if (conditionResult instanceof Boolean) {
       return (Boolean) conditionResult;
@@ -80,7 +83,7 @@ public class WhileStmtExecutor extends StmtExecutor {
     } else if (conditionResult instanceof Float) {
       return ((Float) conditionResult) == 0.0;
     } else if (conditionResult instanceof String) {
-      return (String)conditionResult != "";
+      return (String) conditionResult != "";
     } else {
       return true;
     }
