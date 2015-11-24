d96e0f3b28b7e9fc371e19a698cdc2a7b4fe82d9
heweidong <heweidong526@gmail.com>
2015-11-24 23:48:27 +0800

    delete message type

--- src/message/MessageType.java
+++ src/message/MessageType.java
@@ -9,8 +9,5 @@ public enum MessageType {
   TOKEN,                // message about tokens that scanned by parser
   SOURCE_LINE,          // message about source line produced by source
   SYNTAX_ERROR,         // message about syntax error produce by ClangErrorHandler in parser
-  PARSER_SUMMARY,       // message about parser summary
   RUNTIME_ERROR,        // message send by backend about runtime error
-  INTERPRETER_SUMMARY,  // message send by interpreter to send message about summary message about interpreter.
-
 }
