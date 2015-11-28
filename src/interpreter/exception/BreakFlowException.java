dfe56d69e880e742f5be44896208bc661d11540d
heweidong <heweidong526@gmail.com>
2015-11-28 23:52:19 +0800

    add break flow exception

---
+++ src/interpreter/exception/BreakFlowException.java
@@ -0,0 +1,11 @@
+package interpreter.exception;
+
+/**
+ * <h1>BreakFlowException</h1>
+ * <p>The control flow exception of break statement.</p>
+ */
+public class BreakFlowException extends SchinusException {
+  public BreakFlowException() {
+    super();
+  }
+}
