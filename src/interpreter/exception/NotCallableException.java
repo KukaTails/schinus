c6f8d68747699eb3c4329d2ba902a581079efacb
heweidong <heweidong526@gmail.com>
2015-11-28 23:50:50 +0800

    add not callable exception

--- src/interpreter/exception/NotCallableException.java
+++ src/interpreter/exception/NotCallableException.java
@@ -4,7 +4,7 @@ package interpreter.exception;
  * <h1>NotCallableException</h1>
  * <h1>Exception will be raised when a object which is not callable is called.</h1>
  */
-public class NotCallableException extends Exception {
+public class NotCallableException extends SchinusException {
   public NotCallableException(String exceptionMessage) {
     super(exceptionMessage);
   }
