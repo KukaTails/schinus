64d54f90e744a2499078b84e6e39cfbc2090aff0
heweidong <heweidong526@gmail.com>
2015-12-19 11:52:11 +0800

    modify parameters exception

--- src/interpreter/exception/ParametersException.java
+++ src/interpreter/exception/ParametersException.java
@@ -1,7 +1,9 @@
 package interpreter.exception;
 
 public class ParametersException extends SchinusException {
-  public ParametersException(String message) {
-    super(message);
+  public ParametersException(String methodName, int expectedArguNumber, int actualArguNumber) {
+    super("function " + methodName + " takes " + expectedArguNumber
+        + " argument" + (expectedArguNumber > 1 ? "s" : " ") + " but "
+        + actualArguNumber + " were given.");
   }
 }
