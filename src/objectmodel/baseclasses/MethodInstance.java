90e3e3d17802848fb03d2f6802339fad5b2a0d2a
heweidong <heweidong526@gmail.com>
2016-03-13 23:21:13 +0800

    add exceptions in method instance

--- src/objectmodel/baseclasses/MethodInstance.java
+++ src/objectmodel/baseclasses/MethodInstance.java
@@ -1,14 +1,17 @@
 package objectmodel.baseclasses;
 
 import static intermediate.ICodeNodeType.RETURN_STATEMENT;
-import static objectmodel.predefined.PredefinedConstant.NONE;
 import static objectmodel.predefined.PredefinedType.FUNCTION;
+import static objectmodel.predefined.PredefinedConstant.NO_PRINT;
 
 import intermediate.ICodeNode;
-import interpreter.exception.ReturnFlowException;
 import objectmodel.dictionary.Dictionary;
 import interpreter.executors.StmtExecutor;
+import interpreter.exception.SchinusException;
+import interpreter.exception.ReturnFlowException;
+import interpreter.exception.ParametersException;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -22,22 +25,22 @@ public class MethodInstance extends Instance {
   ICodeNode funcBodyCode;                   // the intermediate code node of function body
 
   /**
-   * @param methodName the name of method.
+   * @param methodName       the name of method.
    * @param parentDictionary the parent of method.
-   * @param existedEnv the environment which contains the object.
-   * @param parameterCount the number of parameters of the method.
-   * @param parametersName the name of parameters.
-   * @param funcBodyCode the intermediate code node of method.
+   * @param existedEnv       the environment which contains the object.
+   * @param parameterCount   the number of parameters of the method.
+   * @param parametersName   the name of parameters.
+   * @param funcBodyCode     the intermediate code node of method.
    */
-  public MethodInstance(String methodName, Dictionary parentDictionary,
-                        Dictionary existedEnv, int parameterCount, ArrayList<String> parametersName, ICodeNode funcBodyCode) {
-    // all methods are instance of function
+  public MethodInstance(String methodName, Dictionary parentDictionary, Dictionary existedEnv, int parameterCount,
+      ArrayList<String> parametersName, ICodeNode funcBodyCode) {
     super(FUNCTION, new Dictionary(), parentDictionary, existedEnv);
     this.methodName = methodName;
     this.parameterCount = parameterCount;
     this.funcBodyCode = funcBodyCode;
     this.parametersName = parametersName;
-    // method instance has a attribute called "__code__"
+
+    writeAttr("__name__", methodName);
     writeAttr("__code__", funcBodyCode);
   }
 
@@ -46,33 +49,30 @@ public class MethodInstance extends Instance {
    * @return the result of executing method on parameters.
    */
   public Object callMethod(ArrayList<Object> parameters)
-      throws ReturnFlowException {
-    // if the number of parameters doesn't equal to parameterCount,
-    // return None.
+      throws SchinusException, IOException {
+    Object result = NO_PRINT;
+    StmtExecutor statementExecutor = new StmtExecutor();
+
     if (parameters.size() != parameterCount) {
-      return NONE;
+      throw new ParametersException(methodName, parameterCount, parameters.size());
     }
 
-    // put the parameters into dictionary,
-    // and set the field of method to be the existedEnv of parameters
+    // put the parameters into dictionary, and set the
+    // field of method to be the existedEnv of parameters
     for (int i = 0; i < parameterCount; ++i) {
-      Base parameter = (Base)parameters.get(i);
+      Base parameter = (Base) parameters.get(i);
       parameter.setExistedEnv(this.getFields());
       writeAttr(parametersName.get(i), parameter);
     }
 
-    StmtExecutor statementExecutor = new StmtExecutor();
-    Object result = NONE;
     ArrayList<ICodeNode> statements = funcBodyCode.getChildren();
-
     try {
-      // return the result of return statement
       for (ICodeNode statement : statements) {
-        if (statement.getType() == RETURN_STATEMENT) {
+        if (statement.getType() != RETURN_STATEMENT) {
+          statementExecutor.execute(statement, getFields());
+        } else {
           result = statementExecutor.execute(statement, getFields());
           throw new ReturnFlowException(result);
-        } else {
-          statementExecutor.execute(statement, getFields());
         }
       }
     } catch(ReturnFlowException e) {
